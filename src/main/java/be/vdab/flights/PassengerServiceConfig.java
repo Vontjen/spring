f0657a81ad77779293fde2d50d3d163e380b3b2e
Vontjen <vontjen@gmail.com>
2017-09-10 15:04:18 +0200

    commit from home

--- src/main/java/be/vdab/flights/PassengerServiceConfig.java
+++ src/main/java/be/vdab/flights/PassengerServiceConfig.java
@@ -1,7 +1,20 @@
 package be.vdab.flights;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class PassengerServiceConfig {
+
+    private PassengerRepositoryConfig passengerRepositoryConfig;
+
+    @Bean
+    public PassengerRepository passengerRepository(){
+        return new PassengerRepository(passengerRepositoryConfig.list());
+    }
+
+    @Bean
+    public PassengerService passengerService(){
+        return new PassengerService(passengerRepository());
+    }
 }
